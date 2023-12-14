a715d9273268a84bace25c4668f5b6650adf9b42
ValerioCarbone <valeriocarbone10@gmail.com>
2023-12-14 16:59:37 +0100

    ADD - TrainSeeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -18,7 +18,15 @@ class TrainSeeder extends Seeder
             $new_train->departure_station = $faker->city();
             do {
                 $new_train->arrival_station = $faker->city();
-            } while ($new_train->departure_station = $new_train->arrival_station);
+            } while ($new_train->departure_station == $new_train->arrival_station);
+            $new_train->departure_date_time = $faker->dateTimeBetween('-10 week', '+20 week');
+            $new_train->arrival_date_time = $faker->dateTimeBetween('-10 week', '+20 week');
+            // $new_train->arrival_date_time = $faker->dateTimeBetween($new_train->departure_date_time, '+1 day');
+            $new_train->train_id = $faker->numerify('####');
+            $new_train->number_of_carriages = $faker->numberBetween(4, 20);
+            // if ($new_train->departure_date_time)
+            // $new_train->on_time = $faker->randomElement([false, true]);
+            $new_train->save();
         }
     }
 }
